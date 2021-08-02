cce9e2f89aa411b5863d12856de93bb91173be32
jcjmenez <jcjmenez.02@gmail.com>
2021-08-02 11:19:11 +0200

    fixed css and updated fibonacci+

--- src/components/fibonacci/Fibonacci.js
+++ src/components/fibonacci/Fibonacci.js
@@ -7,18 +7,26 @@ function Fibonacci() {
   const [sequence, setSequence] = useState([]);
   const [status, setStatus] = useState('');
 
-  // MAX = 163
+  // MAX = 1478 => INFINITY
   const calculateFib = (number) => {
-    const numbers = [0, 1];
-    if (number < 2 || number > 163) {
-      const s = number < 2 ? 'Number must be greater than 2' : 'Number must be less than 163';
+    if (number < 0 || number > 1478) {
+      const s = number < 0 ? 'Number must be positive' : 'Number must be less than 1478 (Infinity)';
       setStatus(s);
     } else {
       setStatus('');
-      for (let i = 2; i < number; i += 1) {
-        numbers[i] = numbers[i - 2] + numbers[i - 1];
+      if (number < 2) {
+        if (Number(number) === 0) {
+          setSequence([0]);
+        } else {
+          setSequence([0, 1]);
+        }
+      } else {
+        const numbers = [0, 1];
+        for (let i = 2; i <= number; i += 1) {
+          numbers[i] = numbers[i - 2] + numbers[i - 1];
+        }
+        setSequence(numbers);
       }
-      setSequence(numbers);
     }
   };
 
@@ -34,14 +42,16 @@ function Fibonacci() {
       <div className="status">
         <p>{status}</p>
       </div>
-      <div className="input">
-        <p>Input a number to calculate its fibonacci</p>
+      <div className="input-field">
+        <p>Input a number to calculate its fibonacci sequence</p>
         <form onSubmit={(event) => {
           event.preventDefault();
           calculateFib(numberInput);
         }}
         >
           <input
+            placeholder="0"
+            type="number"
             value={numberInput}
             onChange={handleNumberChange}
           />
